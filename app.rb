8b162caa464062757f9bd1b997493f07c2ea6dcd
Travis Exline <t.exline22@gmail.com>
2019-06-29 16:03:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  get '/puppy' do
+  post '/puppy' do
     @puppy - Puppy.new(params['name'], params['breed'], params['months_old'])
     erb :display_puppy
   end
